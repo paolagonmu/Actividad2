be42245d3d59e61b41a9ea4dc8fbc17a707dcd74
SENA <SENA@MEDDFPCPD7F3011.FormacionSena.RED>
2017-05-10 07:31:42 -0500

    prueba 1

---
+++ src/newpackage/PERSONA.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newpackage;
+
+/**
+ *
+ * @author SENA
+ */
+public class PERSONA {
+    private int id;
+    private String nombre;
+    private  UNIVERSIDAD UNIVERSIDAD1;
+    public void setid(int idp)
+    {
+    this.id=idp;
+    }
+    
+    public int getid () 
+    { 
+    return id;  
+    }
+    
+    public void setnombre(String nombrep)
+    {
+    this.nombre=nombrep;
+    }
+    
+    public String getnombre () 
+    { 
+    return nombre;  
+    }
+    
+    public void setUNIVERSIDAD1(UNIVERSIDAD UNIVERSIDAD1p)
+    {
+    this.UNIVERSIDAD1=UNIVERSIDAD1p;
+    }
+            
+    public UNIVERSIDAD UNIVERSIDAD1 () 
+    { 
+    return UNIVERSIDAD1;
+    }
+}
+
---
+++ src/newpackage/UNIVERSIDAD.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newpackage;
+
+/**
+ *
+ * @author SENA
+ */
+
+    public class UNIVERSIDAD {
+    private int id;
+    private String nombre;
+    
+    public void setid(int idp)
+    {
+    this.id=idp;
+    }
+    
+    public int getid () 
+    { 
+    return id;  
+    }
+    
+    public void setnombre(String nombrep)
+    {
+    this.nombre=nombrep;
+    }
+    
+    public String getnombre () 
+    { 
+    return nombre;  
+    }
+}
+
+
