4b475e35692bd6e7994607bbf794081a3403507b
sena <sena@sena-MS-7817>
2017-05-16 09:23:26 -0500

    V.2

---
+++ src/actividad2/Sancion.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package actividad2;
+
+/**
+ *
+ * @author sena
+ */
+public class Sancion {
+    
+}
