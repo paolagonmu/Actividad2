a25d1767b58b654d9a8b7c821502d8708788f813
SENA <SENA@MEDDFPCPD7F3022.FormacionSena.RED>
2017-05-16 08:43:40 -0500

    Parte andrea

---
+++ src/actividad2/ARBITRO.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package actividad2;
+
+/**
+ *
+ * @author SENA
+ */
+public class ARBITRO {
+     private String Nombre;
+     private String Colegio;
+    
+   public void setNombre(String nombreP)
+   {
+     this.Nombre= nombreP;
+   }
+   public String getNombre()
+   {
+    return this.Nombre;
+   }
+   
+   public void setColegio(String colegioP)
+   {
+     this.Colegio= colegioP;
+   }
+   public String getColegio()
+   {
+    return this.Colegio;
+   }
+}
---
+++ src/actividad2/INCIDENCIA.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package actividad2;
+
+/**
+ *
+ * @author SENA
+ */
+public class INCIDENCIA extends PARTIDO{
+    
+    private String Descripcion;
+    
+   public void setDescripcion(String descripcionP)
+   {
+     this.Descripcion= descripcionP;
+   }
+   public String getDescripcion()
+   {
+    return this.Descripcion;
+   }
+}
